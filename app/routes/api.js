64ce93e2c9d6f2a3e02c6d5bf23301073a248404
webmalasi <rndweb20@gmail.com>
2025-01-01 14:14:37 +0530

    git razor update

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,10 +1,55 @@
-const express = require("express");
+import express from "express";
+import crypto from 'crypto'; // For ES Modules
+
 const router = express.Router();
+ import Razorpay from 'razorpay';
+
+const razorpay = new Razorpay({
+    key_id: 'rzp_test_vJSuXkj8TU4T34',
+    key_secret: 'M5Hz7j1Qxz89Dr0XyKlO0xjN',
+  });
 
 router.get("/getUsers",(req,res)=>{
 
     res.json({msg:"ok this is routing get "});
     
+    
 });
 
-module.exports = router;
+router.post('/create-order', async (req, res) => {
+    try {
+      const options = {
+        amount: 10*100, // amount in the smallest currency unit
+        currency: 'INR',
+        receipt: 'receipt_' + Math.random().toString(36).substring(7),
+      };
+  
+      const order = await razorpay.orders.create(options);
+      res.status(200).json(order);
+    } catch (err) {
+      res.status(700).json({ error: err });
+    }
+  });
+
+
+  router.post('/verify-payment', async (req, res) => {
+    try {
+      const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+  
+      const sign = razorpay_order_id + '|' + razorpay_payment_id;
+      const expectedSign = crypto.createHmac('sha256', 'YOUR_RAZORPAY_KEY_SECRET')
+                              .update(sign.toString())
+                              .digest('hex');
+  
+      if (razorpay_signature === expectedSign) {
+        // Payment is verified
+        res.status(200).json({ message: 'Payment verified successfully' });
+      } else {
+        res.status(400).json({ error: 'Invalid payment signature' });
+      }
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+export default router;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require("express");
+// const express = require("express");
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 const app= express();
+app.use(cors())
 app.use(express.json());
 
+dotenv.config();
 const PORT =3009;
-require("dotenv").config();
 
-const usersroutes = require("./app/routes/api");
+import  usersroutes  from "./app/routes/api.js";
+
 app.use("/api",usersroutes);
 app.listen(PORT,()=>{
 
